eb0bc90e1d62d917aa2a4c2e49561fd3ebab072a
Meghan Carter <msc2010@yahoo.com>
2023-03-26 08:40:57 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    return a + b;
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide(a, b) {
+    return a / b;
+  }
+  function makeInt(x) {
+    return parseInt(x,10)
+  }
+  function increment(x) {
+    return (x+1);
+  }
+  function decrement(x) {
+    return (x-1);
+  }
+ function preserveDecimal(x) {
+  return parseFloat(x,10)
+ }
